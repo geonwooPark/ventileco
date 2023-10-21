41cc72572c0cd75d22fc453c25d8afab0cdc758e
박건우 <white0581@naver.com>
2023-10-21 22:17:19 +0900

    Button / Input 컴포넌트 제작

---
+++ app/components/Input.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { IconType } from 'react-icons'
+
+interface InputProps {
+  type: 'text' | 'name' | 'email' | 'password'
+  name: string
+  value: string
+  disabled?: boolean
+  placeholder?: string
+  className?: string
+  icon?: IconType
+  iconAction?: () => void
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function Input({
+  type,
+  name,
+  value,
+  disabled,
+  placeholder,
+  className,
+  onChange,
+  icon: Icon,
+  iconAction,
+}: InputProps) {
+  return (
+    <div className="relative">
+      <input
+        type={type}
+        name={name}
+        value={value}
+        disabled={disabled}
+        placeholder={placeholder}
+        onChange={onChange}
+        className={`px-4 py-3 text-sm text-gray-700 bg-white border border-gray-300 rounded outline-none focus:border-black
+        ${className}
+        `}
+      />
+      {Icon && (
+        <div
+          onClick={iconAction}
+          className="absolute right-3 top-3 cursor-pointer"
+        >
+          <Icon size={20} />
+        </div>
+      )}
+    </div>
+  )
+}
