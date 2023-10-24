d35056fcbfcac3e13039c3ad88bf748d29f55d9e
박건우 <white0581@naver.com>
2023-10-25 02:56:30 +0900

    Logo 컴포넌트 반응형 변경 및 최적화

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,18 +1,23 @@
 'use client'
 
 import Image from 'next/image'
+import logo from '/public/images/logo.png'
 import { useRouter } from 'next/navigation'
 
 export default function Logo() {
   const router = useRouter()
 
   return (
-    <Image
-      alt="Logo"
-      className="cursor-pointer z-10 py-3"
-      height="80"
-      width="80"
-      src="/images/logo.png"
-    />
+    <div className="w-[50px] h-[50px] md:w-[70px] md:h-[70px] cursor-pointer z-50 relative">
+      <Image
+        src={logo}
+        alt="로고이미지"
+        quality={100}
+        placeholder="blur"
+        fill
+        className="w-full h-full object-cover"
+        onClick={() => router.push('/')}
+      />
+    </div>
   )
 }
