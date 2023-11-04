8917ea4bff78dffcec726eded7f635e1f842e70b
박건우 <white0581@naver.com>
2023-11-04 21:20:40 +0900

    Mypage 페이지 완성 및 Tap 컴포넌트 제작

---
+++ app/components/tap/Tap.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import React, { useState } from 'react'
+import FavList from './FavList'
+import CommentList from './CommentList'
+
+const menuArr = [
+  { name: '댓글', content: <CommentList /> },
+  { name: '좋아요', content: <FavList /> },
+]
+
+export default function Tap() {
+  const [currentTap, setCurrentTap] = useState(0)
+  return (
+    <div>
+      <div className="flex gap-2 mb-4">
+        {menuArr.map((menu, i) => {
+          return (
+            <button
+              key={i}
+              onClick={() => setCurrentTap(i)}
+              className={
+                currentTap === i
+                  ? 'border-b-2 border-black'
+                  : 'border-b-2 border-transparent text-gray-400'
+              }
+            >
+              {menu.name}
+            </button>
+          )
+        })}
+      </div>
+      <div>{menuArr[currentTap].content}</div>
+    </div>
+  )
+}
---
+++ app/mypage/page.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import mainBg from '/public/images/main-bg.png'
+import React from 'react'
+import Tap from '../components/tap/Tap'
+import getCurrentUser from '../utils/getCurrentUser'
+
+export default async function page() {
+  const currentUser = await getCurrentUser()
+  return (
+    <>
+      <section className="w-full h-[320px] md:h-[420px]">
+        <div className="my-container h-full text-white text-right flex flex-col justify-center items-end">
+          <div className="w-full h-[320px] md:h-[420px] absolute top-0 left-0 -z-10">
+            <Image
+              src={mainBg}
+              alt="메인 배경이미지"
+              quality={100}
+              fill
+              placeholder="blur"
+              className="object-cover brightness-50"
+            />
+          </div>
+          <h1 className="text-4xl md:text-6xl font-bold mb-5">My Page</h1>
+          <p className="text-sm md:text-base">
+            내 정보 및 활동 내역을 확인해보세요
+          </p>
+        </div>
+      </section>
+
+      <section className="mt-10">
+        <div className="my-container">
+          <div className="text-3xl font-semibold mb-4">
+            {currentUser.name}님 환영합니다
+          </div>
+          <div>
+            <h1 className="md:text-lg mb-3">나의 활동</h1>
+            <div className="mb-10">
+              <Tap />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
