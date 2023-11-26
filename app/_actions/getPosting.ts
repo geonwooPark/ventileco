35afb6b6f291a69b8be2989de350bc7809783c62
박건우 <white0581@naver.com>
2023-11-26 14:03:48 +0900

    feat: detail 페이지 시간 기반 재검증(ISR) 적용

--- app/_actions/getPosting.ts
+++ app/_actions/getPosting.ts
@@ -1,8 +1,11 @@
+import { cache } from 'react'
 import { PostingType } from '../_interfaces/interface'
 import { connectMongo } from '../_utils/database'
 import { Posting } from '@/models/posting'
 
-export default async function getPosting(postingId: string) {
+export const revalidate = 600
+
+export default cache(async function getPosting(postingId: string) {
   try {
     await connectMongo()
     const posting = await Posting.findOne<PostingType>({ _id: postingId })
@@ -11,4 +14,4 @@ export default async function getPosting(postingId: string) {
   } catch (error) {
     return null
   }
-}
+})
