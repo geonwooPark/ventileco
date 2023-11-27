312eeec7736ff1f31a79596b8554f521e78e40b2
박건우 <white0581@naver.com>
2023-11-27 21:15:10 +0900

    refactor: middleware 코드 수정

--- middleware.ts
+++ middleware.ts
@@ -7,17 +7,19 @@ const secret = process.env.NEXT_PUBLIC_JWT_SECRET
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request, secret })
-  if (token === null) {
-    return NextResponse.redirect(new URL('/', request.url))
-  }
-  if (request.nextUrl.pathname.startsWith('/write')) {
-    if (token.email !== 'white0581@naver.com') {
+  if (request.nextUrl.pathname.startsWith('/mypage')) {
+    if (token === null) {
       return NextResponse.redirect(new URL('/', request.url))
     }
   }
-}
-
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ['/edit/:path*', '/mypage', '/write'],
+  if (request.nextUrl.pathname.startsWith('/edit')) {
+    if (token?.role !== 'admin') {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
+  if (request.nextUrl.pathname.startsWith('/write')) {
+    if (token?.role !== 'admin') {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
 }
