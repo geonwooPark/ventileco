ab7e8b9e68c53ad241d3b5b23f6fdf299a4c9636
박건우 <white0581@naver.com>
2023-12-04 17:43:49 +0900

    feat: 동적 sitemap 생성

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,9 @@
 import getAllListing from './_actions/getAllListing'
+import getAllListingCount from './_actions/getAllListingCount'
+import getCategoryListingCount from './_actions/getCategoryListingCount'
+import { categories } from './_utils/categoryArr'
+
+const LIMIT = 5
 
 export default async function sitemap() {
   const baseURL = 'https://ventileco-blog.vercel.app'
@@ -7,29 +12,41 @@ export default async function sitemap() {
     page: 1,
     limit: 100,
   })
-
-  const postURLs = listing.map((listingItem) => ({
+  const detailURLs = listing.map((listingItem) => ({
     url: `${baseURL}/detail/${listingItem._id}`,
-    lastModified: listingItem.updatedAt,
+    lastModified: new Date(),
   }))
+
+  const listingCount = await getAllListingCount()
+  const lastPageNum = Math.ceil(listingCount / LIMIT)
+  const postingsURLs = Array.from({ length: lastPageNum - 1 }).map((_, i) => ({
+    url: `${baseURL}/postings/${i + 2}`,
+    lastModified: new Date(),
+  }))
+
+  const categoryURLs = categories.map((category) => ({
+    url: `${baseURL}/categories/${category}`,
+    lastModified: new Date(),
+  }))
+
+  let categoryPageURLs: { url: string; lastModified: Date }[] = []
+  for (const category of categories) {
+    const listingCount = await getCategoryListingCount(category)
+    const lastPageNum = Math.ceil(listingCount / LIMIT)
+    const res = Array.from({ length: lastPageNum - 1 }).map((_, i) => {
+      return {
+        url: `${baseURL}/${category}/${i + 2}`,
+        lastModified: new Date(),
+      }
+    })
+    categoryPageURLs = [...categoryPageURLs, ...res]
+  }
 
   return [
     { url: baseURL, lastModified: new Date() },
-    { url: `${baseURL}/categories?search=React.JS`, lastModified: new Date() },
-    { url: `${baseURL}/categories?search=Next.JS`, lastModified: new Date() },
-    {
-      url: `${baseURL}/categories?search=TypeScript`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseURL}/categories?search=컴퓨터과학`,
-      lastModified: new Date(),
-    },
-    {
-      url: `${baseURL}/categories?search=라이브러리`,
-      lastModified: new Date(),
-    },
-
-    ...postURLs,
+    ...detailURLs,
+    ...postingsURLs,
+    ...categoryURLs,
+    ...categoryPageURLs,
   ]
 }
