5a9713c560a6d5c7906a81d73eb6dfbc14878bec
박건우 <white0581@naver.com>
2023-12-14 23:36:14 +0900

    chore: 테스트 서버 배포

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import EmptyState from '../components/common/EmptyState'
 
 export default function page() {
-  return <EmptyState label="준비중..." />
+  return <EmptyState label="헌재 준비중 입니다." />
 }
