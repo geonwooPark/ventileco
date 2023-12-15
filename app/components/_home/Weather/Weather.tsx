c7d2f6d8e266be54354c818195d2ce28bb280246
박건우 <white0581@naver.com>
2023-12-15 21:00:54 +0900

    refactor: Home 페이지 SSR + React-Query 적용

--- app/components/_home/Weather/Weather.tsx
+++ app/components/_home/Weather/Weather.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import WeatherInfo from './WeatherInfo'
+import getQueryClient from '@/app/actions/getQueryClient'
+import getData from '@/app/actions/getData'
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
 
-export default function Weather() {
+export const CITY = 'Seoul'
+export const APIKEY = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY
+
+export default async function Weather() {
+  const queryClient = getQueryClient()
+  await queryClient.prefetchQuery({
+    queryKey: ['weather'],
+    queryFn: () =>
+      getData<any>(
+        `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${APIKEY}&units=metric`,
+      ),
+  })
+  const dehydratedState = dehydrate(queryClient)
+
   return (
     <div className="h-full">
       <h4 className="mb-2 text-lg font-medium">날씨</h4>
-      <WeatherInfo />
+      <HydrationBoundary state={dehydratedState}>
+        <WeatherInfo />
+      </HydrationBoundary>
     </div>
   )
 }
