4b7092feab5421fe1dc999ba83499c4880fae743
박건우 <white0581@naver.com>
2023-12-17 02:13:11 +0900

    chore: 기타 코드 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Weather from './components/_home/Weather/Weather'
 import MyCheckList from './components/_home/MyCheckList/MyCheckList'
 import LoadingUI from './components/_home/LoadingUI'
 
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
   return (
     <section className="flex h-auto min-h-full items-center py-[82px] md:py-[102px]">
