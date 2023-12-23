85f881f5349ea1a96b28cc0424c79869d1ac5d4a
박건우 <white0581@naver.com>
2023-12-23 20:29:31 +0900

    style: StageFive 컴포넌트 내부 바로가기 링크 넓이 수정

--- app/components/_about/StageContainer/Stage/StageFive.tsx
+++ app/components/_about/StageContainer/Stage/StageFive.tsx
@@ -7,24 +7,24 @@ import Container from '../../../common/Container'
 export default function StageFive() {
   return (
     <section className="flex h-full w-full items-center justify-center">
-      <Container>
+      <Container className="flex flex-col items-center">
         <div className="mb-2 sm:mb-4 lg:mb-6">
           <span className="mr-3 sm:mr-4 lg:mr-6">더 많은</span>
           <HighLight beforeBgColor="before:bg-orange-400">프로젝트</HighLight>
           <span>가 궁금하시면</span>
         </div>
+
         <div className="mb-2 sm:mb-4 lg:mb-6">
           <span>아래에 방문해주세요.</span>
         </div>
+
         <Link
           href={'https://github.com/geonwooPark'}
           target="_black"
-          className="flex items-center justify-center text-base text-blue-600 duration-300 hover:opacity-70"
+          className="flex items-center text-base text-blue-600 duration-300 hover:opacity-70"
         >
-          <span>바로가기</span>
-          <span>
-            <MdKeyboardArrowRight />
-          </span>
+          바로가기
+          <MdKeyboardArrowRight size={20} className="mt-1" />
         </Link>
       </Container>
     </section>
