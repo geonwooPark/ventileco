6ae32ae9af6ad536309f36c916f9b4b99e3c339b
박건우 <white0581@naver.com>
2024-01-04 11:35:46 +0900

    Feat: 카카오 맵 API 설정

---
+++ app/components/_hot-place/Map.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { useMapActions } from '@/app/hooks/store/useMap'
+import React, { useEffect, useRef } from 'react'
+import Markers from './Markers'
+
+export default function Map() {
+  const { onAdd: addMap } = useMapActions()
+  const container = useRef(null)
+
+  useEffect(() => {
+    window.kakao.maps.load(async () => {
+      const options = {
+        center: new window.kakao.maps.LatLng(37.574187, 126.976882),
+        level: 5,
+      }
+      const map = new window.kakao.maps.Map(container.current, options)
+      addMap(map)
+    })
+  }, [])
+
+  return (
+    <div ref={container} className="h-full w-full">
+      <Markers />
+    </div>
+  )
+}
