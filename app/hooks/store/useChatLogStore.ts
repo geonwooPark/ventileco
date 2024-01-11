10cc559d002d9233cbf2e39ecc46e475f344324c
박건우 <white0581@naver.com>
2024-01-11 23:24:03 +0900

    Feat: GPT 공지사항 추가

--- app/hooks/store/useChatLogStore.ts
+++ app/hooks/store/useChatLogStore.ts
@@ -24,6 +24,12 @@ const useChatLogStore = create<State & Actions>()((set) => ({
       content: '저는 토큰 문제로 이전 대화를 기억하진 못해요. 참고해주세요!',
       sender: 'gpt',
     },
+    {
+      id: 2,
+      content:
+        'Vercel 무료 버전으로 배포해서 10초으로 이상으로 답변이 길어지면 에러나요ㅜㅜ ',
+      sender: 'gpt',
+    },
   ],
   actions: {
     onClear: () => set({ chatLog: [] }),
