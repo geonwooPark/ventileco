1661cfc292dec284df79cd9b6b409c502e383398
박건우 <white0581@naver.com>
2024-01-25 23:45:58 +0900

    Feat: useMouseCoordinates 커스텀 훅 제작

--- app/components/common/RabbitGPT/RabbitGPT.tsx
+++ app/components/common/RabbitGPT/RabbitGPT.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import ChatContainer from './ChatContainer/ChatContainer'
 import {
   RabbitBody,
@@ -8,6 +8,7 @@ import {
   RabbitEyeRight,
 } from '../../../../public/svgs'
 import useScaleAnimation from '@/hooks/useScaleAnimation'
+import useMouseCoordinates from '@/hooks/useMouseCoordinates'
 
 interface rotateVariantsType {
   [key: number]: string
@@ -27,39 +28,19 @@ const rotateVariants: rotateVariantsType = {
 }
 
 export default function RabbitGPT() {
-  const { isOpen, scale, setIsOpen } = useScaleAnimation()
   const [deg, setDeg] = useState(0)
+
+  const { isOpen, scale, setIsOpen } = useScaleAnimation()
+  const { OriginRef } = useMouseCoordinates(setDeg)
 
   const onClick = () => {
     setIsOpen((prev) => !prev)
   }
 
-  const OriginRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const onMouseMove = (e: MouseEvent) => {
-      if (!OriginRef.current) return
-
-      const { x: x1, y: y1 } = e
-      const { x: x2, y: y2 } = OriginRef.current.getBoundingClientRect()
-
-      let rad = Math.atan2(y2 - y1, x2 - x1)
-      if (rad < 0) rad += Math.PI * 2
-      rad = (rad * 180) / Math.PI - (((rad * 180) / Math.PI) % 10)
-
-      setDeg(rad)
-    }
-
-    window.addEventListener('mousemove', onMouseMove)
-    return () => {
-      window.removeEventListener('mousemove', onMouseMove)
-    }
-  })
-
   return (
     <div
       onClick={onClick}
-      className={`fixed bottom-0 left-0 z-[200] hidden cursor-pointer duration-200 ease-in-out md:block ${
+      className={`fixed bottom-0 left-0 z-[50] hidden cursor-pointer duration-200 ease-in-out md:block ${
         isOpen ? 'translate-y-[0]' : 'translate-y-[120px]'
       }`}
     >
