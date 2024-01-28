9f0c295bae636c9aadd1283a503a932b057628bf
박건우 <white0581@naver.com>
2024-01-28 17:15:02 +0900

    Style: SkeletonStoreList 스타일 변경

--- app/components/_hot-place/Map/StoreListings/SideInfo/StoreList/SkeletonStoreList.tsx
+++ app/components/_hot-place/Map/StoreListings/SideInfo/StoreList/SkeletonStoreList.tsx
@@ -9,9 +9,9 @@ export default function SkeletonStoreList() {
           className="animate-pulse cursor-pointer p-2 duration-100 last:mb-0 hover:bg-white/80"
         >
           <div className="flex gap-4">
-            <div className="relative h-[60px] w-[90px] bg-slate-300"></div>
+            <div className="relative h-[60px] w-[90px] shrink-0 bg-slate-300"></div>
             <div className="w-full">
-              <div className="mb-0.5 h-5 w-[70%] rounded-md bg-slate-300"></div>
+              <div className="mb-0.5 h-5 w-full rounded-md bg-slate-300"></div>
               <div className="h-4 w-[30%] rounded-md bg-slate-300"></div>
             </div>
           </div>
