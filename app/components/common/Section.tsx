3bf7bd3f3687f6b4f4c87abbc71357e4f48c789a
박건우 <white0581@naver.com>
2024-02-04 15:52:35 +0900

    Chore: Section 컴포넌트 코드 수정

--- app/components/common/Section.tsx
+++ app/components/common/Section.tsx
@@ -16,7 +16,7 @@ export default function Section({
   return (
     <section className={`bg-white pb-20 ${className}`} key={innerKey}>
       <Container>
-        <h3 className="mb-4 text-lg font-medium">{label}</h3>
+        {label && <h3 className="mb-4 text-lg font-medium">{label}</h3>}
         {children}
       </Container>
     </section>
