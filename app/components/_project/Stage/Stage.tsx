b0f568de0afaedd1f3698ab307020da4e5fcded9
박건우 <white0581@naver.com>
2024-04-06 17:22:46 +0900

    Style: 프로젝트 페이지 스타일 변경

--- app/components/_project/Stage/Stage.tsx
+++ app/components/_project/Stage/Stage.tsx
@@ -15,25 +15,27 @@ export default function Stage({ title, image, description, link }: StageProps) {
   return (
     <section className="flex h-full w-full items-center justify-center">
       <Container>
-        <div className="space-y-5">
-          <div className="text-center text-lg">
-            <HighLight beforeBgColor="before:bg-blue-400 font-bold">
-              {title}
-            </HighLight>
-          </div>
+        <div className="mb-5 text-lg">
+          <HighLight beforeBgColor="before:bg-blue-400 font-bold">
+            {title}
+          </HighLight>
+        </div>
+        <div className="h-full space-y-4 lg:flex lg:items-start lg:gap-4 lg:space-y-0">
           <div
-            className={`relative mx-auto aspect-[16/9] w-full overflow-hidden rounded-lg border border-gray-700 bg-cover bg-center bg-no-repeat lg:w-[70%] 2xl:w-full ${image}`}
+            className={`relative mx-auto aspect-[16/9] w-full overflow-hidden rounded-lg border border-gray-700 bg-cover bg-center bg-no-repeat lg:w-[600px] lg:shrink-0 ${image}`}
           />
-          <p className="text-sm">{description}</p>
-          <div className="flex justify-center">
-            <Link
-              href={link as any}
-              target="_black"
-              className="flex items-center justify-center text-blue-600 duration-300 hover:opacity-70"
-            >
-              바로가기
-              <MdKeyboardArrowRight size={20} className="mt-1" />
-            </Link>
+          <div>
+            <p className="mb-4">{description}</p>
+            <div className="flex justify-center">
+              <Link
+                href={link as any}
+                target="_black"
+                className="flex items-center justify-center text-blue-600 duration-300 hover:opacity-70"
+              >
+                바로가기
+                <MdKeyboardArrowRight size={20} className="mt-1" />
+              </Link>
+            </div>
           </div>
         </div>
       </Container>
