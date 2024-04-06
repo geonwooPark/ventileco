d9c844d7b8e0f5e163bbc460f63eb48447fc1a1c
박건우 <white0581@naver.com>
2024-04-06 18:45:57 +0900

    Rename: 인트로 MenAnimation 제거

--- app/utils/setSessionStorageWithExpire.ts
+++
@@ -1,6 +0,0 @@
-import dayjs from '@/lib/dayjs'
-
-export const setSessionStorageWithExpire = (key: string, value: any) => {
-  const values = { value, expires: dayjs() }
-  sessionStorage.setItem(key, JSON.stringify(values))
-}
