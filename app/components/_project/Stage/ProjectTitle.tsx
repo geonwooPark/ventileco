319c18ed686a4815d04cdf6aa79833bbbe8414ce
박건우 <white0581@naver.com>
2024-04-07 18:12:28 +0900

    Style: Project 페이지 스타일 리뉴얼

---
+++ app/components/_project/Stage/ProjectTitle.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+interface ProjectTitleProps {
+  title: string
+}
+
+export default function ProjectTitle({ title }: ProjectTitleProps) {
+  return (
+    <div className="mb-2 flex h-[80px] items-center justify-center overflow-hidden">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 660 150"
+        className="-translate-y-6"
+      >
+        <path
+          id="curve4"
+          d="M2.84,144.27s128.97-48.27,327.16-48.27,327.16,48.27,327.16,48.27"
+          fill="none"
+          stroke="none"
+          strokeMiterlimit="10"
+        ></path>
+        <text textAnchor="middle">
+          <textPath
+            className="text-center font-point text-3xl tracking-widest"
+            xlinkHref="#curve4"
+            fill="currentColor"
+            startOffset="51%"
+          >
+            {title}
+          </textPath>
+        </text>
+      </svg>
+    </div>
+  )
+}
--- app/components/_project/Stage/Stage.tsx
+++ app/components/_project/Stage/Stage.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import HighLight from './HighLight'
 import Container from '@common/Container'
 import Link from 'next/link'
-import { MdKeyboardArrowRight } from 'react-icons/md'
+import ProjectTitle from './ProjectTitle'
 
 interface StageProps {
   title: string
@@ -15,14 +14,10 @@ export default function Stage({ title, image, description, link }: StageProps) {
   return (
     <section className="flex h-full w-full items-center justify-center">
       <Container>
-        <div className="mb-5 text-lg">
-          <HighLight beforeBgColor="before:bg-blue-400 font-bold">
-            {title}
-          </HighLight>
-        </div>
+        <ProjectTitle title={title} />
         <div className="h-full space-y-4 lg:flex lg:items-start lg:gap-4 lg:space-y-0">
           <div
-            className={`relative mx-auto aspect-[16/9] w-full overflow-hidden rounded-lg border border-gray-700 bg-cover bg-center bg-no-repeat lg:w-[600px] lg:shrink-0 ${image}`}
+            className={`card-shadowed relative mx-auto aspect-[16/9] w-full rounded-md bg-cover bg-center bg-no-repeat lg:w-[600px] lg:shrink-0 ${image}`}
           />
           <div>
             <p className="mb-4">{description}</p>
@@ -30,10 +25,9 @@ export default function Stage({ title, image, description, link }: StageProps) {
               <Link
                 href={link as any}
                 target="_black"
-                className="flex items-center justify-center text-blue-600 duration-300 hover:opacity-70"
+                className="button-shadowed flex items-center justify-center px-6 py-3 font-point text-brown-dark duration-300"
               >
-                바로가기
-                <MdKeyboardArrowRight size={20} className="mt-1" />
+                Visit Now
               </Link>
             </div>
           </div>
