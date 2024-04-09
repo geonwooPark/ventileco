40db076fb59043d600cc241beccecdc66b7fea49
geonwooPark <white0581@naver.com>
2024-04-09 20:51:31 +0900

    Fix: 블로그 슬라이드 드래그시 클릭 이벤트 막기

--- app/hooks/useDragEvent.ts
+++ app/hooks/useDragEvent.ts
@@ -1,34 +1,60 @@
 import { throttle } from '@/utils/throttle'
-import { useRef, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
 export default function useDragEvent() {
   const dragContainer = useRef<HTMLDivElement>(null)
 
   const [isDragging, setIsDragging] = useState(false)
   const [startPoint, setStartPoint] = useState(0)
-  const [scrollPosition] = useState(0)
+  const [totalX, setTotalX] = useState(0)
+
+  const preventUnexpectedEffects = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      e.preventDefault()
+      e.stopPropagation()
+    },
+    [],
+  )
 
   const onDragStart = (e: React.MouseEvent<HTMLDivElement>) => {
-    // e.preventDefault()
-    if (dragContainer.current) {
-      setIsDragging(true)
-      setStartPoint(e.pageX + dragContainer.current?.scrollLeft)
+    preventUnexpectedEffects(e)
+    setIsDragging(true)
+    const x = e.clientX
+    setStartPoint(x)
+    if (dragContainer.current && 'scrollLeft' in dragContainer.current) {
+      setTotalX(x + dragContainer.current.scrollLeft)
+    }
+  }
+
+  const onDragMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!isDragging) return
+    preventUnexpectedEffects(e)
+
+    const scrollLeft = totalX - e.clientX
+
+    if (dragContainer.current && 'scrollLeft' in dragContainer.current) {
+      dragContainer.current.scrollLeft = scrollLeft
     }
   }
 
   const onDragEnd = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault()
-    setIsDragging(false)
-  }
-
-  const onDragMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault()
-
     if (!isDragging) return
-    const dragLength = e.pageX - startPoint
+    if (!dragContainer.current) return
 
-    if (dragContainer.current) {
-      dragContainer.current.scrollLeft = scrollPosition - dragLength
+    setIsDragging(false)
+
+    const endX = e.clientX
+    const childNodes = [...(dragContainer.current?.childNodes || [])]
+    const dragDiff = Math.abs(startPoint - endX)
+
+    if (dragDiff > 10) {
+      childNodes.forEach((child) => {
+        child.addEventListener('click', preventUnexpectedEffects as any)
+      })
+    } else {
+      childNodes.forEach((child) => {
+        child.removeEventListener('click', preventUnexpectedEffects as any)
+      })
     }
   }
 
@@ -39,6 +65,5 @@ export default function useDragEvent() {
     onDragStart,
     onDragEnd,
     onDragMove: onThrottleDragMove,
-    isDragging,
   }
 }
