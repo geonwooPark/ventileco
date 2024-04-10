84773eeea9e9accdad1f771b0ea14d3ac1ebfea8
geonwooPark <white0581@naver.com>
2024-04-10 12:18:50 +0900

    Chore: HeaderDecoration 작업

--- app/components/common/Header/Header.tsx
+++ app/components/common/Header/Header.tsx
@@ -6,19 +6,22 @@ import RightSide from './Rightside/RightSide'
 import { usePathname } from 'next/navigation'
 import HomeButton from './HomeButton'
 import LeftSide from './Leftside/LeftSide'
+import HeaderDecoration from './HeaderDecoration'
 
 export default function Header() {
   const path = usePathname()
 
   return (
     <header
-      className={`fixed top-0 z-[100] h-[64px] w-full bg-beige-light font-normal shadow-md transition duration-200`}
+      className={`fixed top-0 z-[100] h-[56px] w-full bg-beige-light font-normal shadow-md transition duration-200`}
     >
       <Container className="flex h-full items-center justify-between">
         <LeftSide path={path} />
         <HomeButton />
         <RightSide path={path} />
       </Container>
+
+      {/* <HeaderDecoration /> */}
     </header>
   )
 }
--- app/components/common/Header/HeaderDecoration.tsx
+++ app/components/common/Header/HeaderDecoration.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { HeaderDeco } from '../../../../public/svgs'
 
 export default function HeaderDecoration() {
   return (
-    <div className="absolute left-0 top-[64px]">
-      <HeaderDeco />
+    <div>
+      <div className="absolute left-0 top-0">
+        <div className="absolute left-[-40px] h-[120px] w-[120px] rounded-full bg-beige-light" />
+        <div className="h-screen w-5 bg-beige-light"></div>
+      </div>
+      <div className="absolute right-0 top-0 -scale-x-100">
+        <div className="absolute right-[-60px] h-[120px] w-[120px] rounded-full bg-beige-light" />
+        <div className="h-screen w-5 bg-beige-light"></div>
+      </div>
     </div>
   )
 }
