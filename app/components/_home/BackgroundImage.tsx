863312a4d78201915fc4b3811c081b36313c0eb1
geonwooPark <white0581@naver.com>
2024-04-22 15:49:32 +0900

    Style: Home 페이지 배경화면 설정

--- app/components/_home/BackgroundImage.tsx
+++ app/components/_home/BackgroundImage.tsx
@@ -5,12 +5,14 @@ import bg from '../../../public/images/bg.png'
 
 export default function BackgroundImage() {
   return (
-    <div className="fixed inset-0 h-full w-full">
+    <div className="fixed inset-0 z-[-10] h-full w-full">
       <Image
         src={bg}
         alt="bg"
+        priority
+        fill
         placeholder="blur"
-        className="h-full w-full object-cover object-center opacity-50"
+        className="h-full w-full object-cover opacity-50"
       />
       <DashedBox />
     </div>
