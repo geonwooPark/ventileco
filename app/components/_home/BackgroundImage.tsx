aadc3d722d00a0055ad54ac6feb847c7605326b4
geonwooPark <white0581@naver.com>
2024-04-22 19:20:46 +0900

    Feat: Home페이지 스크롤 이벤트 추가

--- app/components/_home/BackgroundImage.tsx
+++ app/components/_home/BackgroundImage.tsx
@@ -1,9 +1,44 @@
+'use client'
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
 import DashedBox from '../common/DashedBox'
 import bg from '../../../public/images/bg.png'
 
 export default function BackgroundImage() {
+  useEffect(() => {
+    const toScroll = (e: WheelEvent) => {
+      e.preventDefault()
+
+      if (e.deltaY < 0) {
+        if (window.scrollY === 0) return
+
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth',
+        })
+      } else {
+        if (window.innerHeight + window.scrollY >= document.body.scrollHeight)
+          return
+
+        window.scrollTo({
+          top: window.innerHeight - 56,
+          left: 0,
+          behavior: 'smooth',
+        })
+      }
+    }
+
+    window.addEventListener('wheel', toScroll, {
+      passive: false,
+    })
+
+    return () => {
+      window.removeEventListener('wheel', toScroll)
+    }
+  }, [])
+
   return (
     <div className="fixed inset-0 z-[-10] h-full w-full">
       <Image
