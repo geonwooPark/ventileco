00056f02dffb912da3ee1f502d55eb4b04e0d359
geonwooPark <white0581@naver.com>
2024-06-12 17:51:29 +0900

    Chore: Loader 컴포넌트 변경

--- app/components/_home/Three/Loader.tsx
+++ app/components/_home/Three/Loader.tsx
@@ -12,20 +12,7 @@ export default function Loader() {
         progress === 100 && 'hidden'
       }`}
     >
-      <div className="space-y-4">
-        <Image src={Gun} alt="gun" />
-        <div className="relative mx-auto h-[10px] w-[240px] overflow-hidden rounded-full bg-white">
-          <div
-            className="absolute inset-0 bg-brown-dark"
-            style={{
-              width: `${progress}%`,
-            }}
-          />
-        </div>
-        <div className="text-center text-lg text-white">
-          {Math.floor(progress)}%
-        </div>
-      </div>
+      <Image src={Gun} alt="gun" style={{ opacity: progress / 100 }} />
     </div>
   )
 }
