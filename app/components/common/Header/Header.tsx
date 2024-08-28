273795c798d69754e655d14d2a5a4cf3a08ebc9f
박건우 <white0581@naver.com>
2024-08-28 22:48:44 +0900

    Chore: jenkins 테스트 중

--- app/components/common/Header/Header.tsx
+++ app/components/common/Header/Header.tsx
@@ -7,7 +7,7 @@ import Logo from './Logo'
 export default function Header() {
   return (
     <header
-      className={`fixed top-0 z-[100] h-[56px] w-full bg-beige-light font-normal shadow-md transition duration-200`}
+      className={`fixed top-0 z-[100] h-[56px] w-full bg-red-400 font-normal shadow-md transition duration-200`}
     >
       <Container className="flex h-full items-center justify-between">
         <LeftSide />
