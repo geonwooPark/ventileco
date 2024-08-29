bb3c95d6b07844243979635c3784ef1c4eac23d7
박건우 <white0581@naver.com>
2024-08-30 02:27:45 +0900

    Chore: Jenkins 테스트ㅜㅈㅇ

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import EmptyState from './components/common/EmptyState'
 export default function NotFound() {
   return (
     <div className="h-[100vh] bg-gray-700">
-      <EmptyState label="404 존재하지 않는 페이지입니다!" />
+      <EmptyState label="404 존재하지 않는 페이지입니다." />
     </div>
   )
 }
